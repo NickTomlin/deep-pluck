7c6da666b95c255c46997d2cc2b0d9dc8ab4db84
Nick Tomlin <nick.tomlin@gmail.com>
2014-06-13 14:16:41 -0500

    Add browserify build step

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
 var _ = require('lodash');
 var mocha = require('gulp-mocha');
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var jshint = require('gulp-jshint');
 var karma = require('gulp-karma');
+
+var MAIN_FILE = 'pluck-deep.js';
+var MODULE_NAME = 'pluckDeep';
 
 function clientTest() {
   // Invoking with func.bind sets context to a config object.
@@ -32,6 +39,28 @@ gulp.task('test:server', function() {
       throw err;
     });
 });
+
+gulp.task('jshint', function () {
+  return gulp.src([MAIN_FILE, 'test/*-test.js'])
+  .pipe(jshint());
+});
+
+gulp.task('browserify', function () {
+  return gulp.src(MAIN_FILE)
+  .pipe(browserify({
+    standalone: MODULE_NAME
+  }))
+  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('uglify', ['browserify'], function () {
+  return gulp.src('dist/' + MAIN_FILE)
+  .pipe(uglify())
+  .pipe(rename(MAIN_FILE.replace('.js', '.min.js')))
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', ['jshint', 'browserify', 'uglify']);
 
 gulp.task('test', ['test:client', 'test:server']);
 gulp.task('default', clientTest.bind({'action': 'watch'}));
