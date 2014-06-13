c327ffdd46734dd2de9074797e7f7601fd00846a
Nick Tomlin <nick.tomlin@gmail.com>
2014-06-13 12:14:00 -0500

    switch to cjs syntax

--- pluck-deep.js
+++ pluck-deep.js
@@ -1,33 +1,22 @@
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else if (typeof exports === 'object') {
-        module.exports = factory();
-    } else {
-        root.pluckDeep = factory();
-  }
-}(this, function () {
+var isObject = pluckDeep._isObject = function(target) {
+  var t = typeof target;
+  return (t === 'function' || t === 'object') || false;
+};
 
-  var isObject = pluckDeep._isObject = function(target) {
-    var t = typeof target;
-    return (t === 'function' || t === 'object') || false;
-  };
+function pluckDeep (source, target, results) {
+  results = results || [];
 
-  function pluckDeep (source, target, results) {
-    results = results || [];
-
-    if (source[target]) {
-      results.push(source[target]);
-    }
-
-    if (isObject(source)) {
-      for (var prop in source) {
-        pluckDeep(source[prop], target, results);
-      }
-    }
-
-    return results;
+  if (source[target]) {
+    results.push(source[target]);
   }
 
-  return pluckDeep;
-}));
+  if (isObject(source)) {
+    for (var prop in source) {
+      pluckDeep(source[prop], target, results);
+    }
+  }
+
+  return results;
+}
+
+module.exports = pluckDeep;
