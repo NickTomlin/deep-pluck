e1d830d14a6c39237d678e59052eca2c502052c7
Nick Tomlin <nick.tomlin@gmail.com>
2014-06-13 14:46:27 -0500

    Add browserify to karma

--- karma-shared.conf.js
+++ karma-shared.conf.js
@@ -1,11 +1,12 @@
 module.exports = function(config) {
   config.set({
     basePath: '',
-    frameworks: ['mocha', 'expect'],
+    frameworks: ['mocha', 'browserify'],
     // i'm not sure if this is going to get overwritten or merged
     files: ['pluck-deep.js','test/*-test.js','test/fixtures/*'],
     preprocessors: {
-      '**/*.json': 'html2js'
+      '**/*.json': 'html2js',
+      'test/*.js': 'browserify'
     },
     exclude: [],
     reporters: ['progress'],
